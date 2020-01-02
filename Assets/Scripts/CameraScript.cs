f6e941c4f36eb49601c6bda89715b7b954d36b69
Alexis Bugielski <alexis.bugielski@gmail.com>
2020-01-02 22:37:16 +0100

    Add level
    build a room for the building B

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private GameObject player;
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        transform.position = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position;
+        //transform.rotation = player.transform.rotation;
+        float mouse_x = Input.GetAxis("Mouse X");
+        float mouse_y = -1 * Input.GetAxis("Mouse Y");
+        float mouse_z = Input.mousePosition.z;
+        Vector3 rotation = new Vector3(mouse_y, mouse_x, 0);
+        transform.eulerAngles += rotation; // Update rotation with the mouse location. Don't use Rotate
+        player.transform.eulerAngles += new Vector3(0, mouse_x, 0);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+               
+                Debug.Log("OK");
+                //Transform objectHit = hit.transform;
+                Vector3 point = hit.point;
+                //Vector3 position = new Vector3(objectHit.position.x, 1f, objectHit.position.z);
+                Vector3 position = new Vector3(point.x, 1f, point.z);
+                player.transform.position = position;
+
+                // Do something with the object that was hit by the raycast.
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DeplacementScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacementScript : MonoBehaviour
+{
+    private GameObject camera;
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.FindWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(Time.deltaTime * x * speed, .0f, Time.deltaTime * z * speed);
+
+        transform.Translate(movement);
+    }
+}
